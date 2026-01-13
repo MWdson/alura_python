f009d4c185dc0b477a53c42a09d474b9e9818993
MWdson <mwdsonss@gmail.com>
2026-01-13 09:01:10 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,6 +1,9 @@
-teste = {}
+import json
+with open("dados.json", "r", encoding="utf-8") as arquivo:
+    conteudo = json.load(arquivo)["listBoxesWithSomeClientOffline"]
 
-if teste:
-    print("true")
-else:
-    print("false")
+quantidade = sum(x["offline"] for x in conteudo)
+
+print(quantidade)
+
+
